1a296e3a42e0e38dc9a0d589fcbee2252492d353
S16-sakthi <logeshlogesh212@gmail.com>
2025-02-20 16:02:36 +0530

    Add files via upload

---
+++ app/app.component.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet,RouterLink],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+
+
+}
---
+++ app/app.routes.ts
@@ -0,0 +1,23 @@
+import { Routes } from '@angular/router';
+import {TodoComponent } from './component/todo/todo.component';
+import { AboutComponent } from './about/about.component';
+
+
+export const routes: Routes = [
+    {	
+        path:'',
+    component: TodoComponent,
+    pathMatch:'full'
+    },
+    
+    {
+        path:'home',
+    component: TodoComponent,
+    pathMatch:'full'
+    },
+    {
+        path:'about',
+    component: AboutComponent,
+    pathMatch:'full'
+    },
+];
